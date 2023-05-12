b77cb16e7dc37a262c2a20c77b6cd2371f1f7ab9
d.larin <larin.dn@icloud.com>
2023-05-13 03:37:56 +0400

    version_1.2

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from classes import HeadHunterAPI, Supperjob, Connector
+from classes import HeadHunterAPI, SupperJob, Connector
 
 
 def main():
+    vacancies_json = []
+
     keyword = "Python"
     hh = HeadHunterAPI(keyword)
-    sj = Supperjob(keyword)
+    sj = SupperJob(keyword)
     for api in (hh, sj):
         api.get_vacancies(pages_count=1)
         vacancies_json.extend(api.get_formatted_vacancies())
@@ -14,12 +16,21 @@ def main():
     while True:
         command = input(
             "1 - Вывести список вакансий; \n"
+            "2 - Сортировка по минимальной зарплате; \n"
+            "3 - Сортировка по минимальной зарплате (DESC); \n"
+            "4 - Сортировка по максимальной зарплате; \n"
             "exit - Выйти. \n"
         )
         if command.lower() == 'exit':
             break
         elif command.lower() == '1':
             vacancies = connector.select()
+        elif command.lower() == '2':
+            vacancies = connector.sorted_vacancies_by_salary_from_asc()
+        elif command.lower() == '3':
+            vacancies = connector.sorted_vacancies_by_salary_from_desc()
+        elif command.lower() == '4':
+            vacancies = connector.sorted_vacancies_by_salary_to_asc()
 
         for vacancy in vacancies:
             print(vacancy, end='\n\n')
